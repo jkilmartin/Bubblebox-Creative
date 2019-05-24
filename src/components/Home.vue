4f7e033677e961ef0a33cc9f9e3a666bc942ead1
jkilmartin <jkilmartin48@gmail.com>
2019-05-24 21:29:31 +1200

    final

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,24 +3,9 @@
     <div class="background">
       <div class="my-container">
         <div class="row">
-          <div class="column col-md-6 col-sm-12 geoff">
-            <router-link :to="'/geoff'">
-              <div class="name">GEOFF</div>
-            </router-link>
-          </div>
-          <div class="column col-md-6 col-sm-12 holly">
-            <router-link :to="'/holly'">
-              <div class="name">HOLLY</div>
-            </router-link>
-          </div>
-          <div class="column col-md-6 col-sm-12 jack">
+          <div class="column col-md-6 col-sm-24 jack">
             <router-link :to="'/jack'">
               <div class="name">JACK</div>
-            </router-link>
-          </div>
-          <div class="column col-md-6 col-sm-12 jeremy">
-            <router-link :to="'/jeremy'">
-              <div class="name">JEREMY</div>
             </router-link>
           </div>
         </div>
@@ -40,19 +25,23 @@ export default {
   width: 100vw;
   height: 100vh;
 }
+
 .my-container {
   height: 100%;
   width: 100%;
   text-align: center;
 }
+
 .home-enter-active,
 .home-leave-active {
   transition: opacity 1s ease-in-out;
 }
+
 .home-enter,
 .home-leave-to {
   opacity: 0;
 }
+
 a {
   height: 100%;
   width: 100%;
@@ -62,48 +51,34 @@ a {
   color: #fff;
   font-size: 5rem;
 }
+
 a:hover {
   text-decoration: none;
 }
+
 .column {
   height: 50vh;
 }
+
 .row {
   margin: 0 !important;
 }
-.geoff,
-.holly,
-.jack,
-.jeremy {
+
+.jack {
   background: black;
   transition: background 0.5s linear;
 }
-.geoff:hover,
-.holly:hover,
-.jack:hover,
-.jeremy:hover {
+
+.jack:hover {
   background: black;
 }
-.geoff:hover {
-  background: url("../assets/geoff/geoff-tiger.jpg") bottom / cover no-repeat;
-}
-.holly:hover {
-  background: url("../assets/holly/holly-tiger.jpg") center / cover no-repeat;
-}
-.jack:hover {
-  background: url('../assets/jack/source.gif') center / cover no-repeat;
-}
-.jeremy:hover {
-  background: url("../assets/jeremy/goose.jpg") center / cover no-repeat;
-  color: black;
-}
+
+
 .name {
   color: #fff;
 }
-.geoff:hover .name,
-.holly:hover .name,
-.jack:hover .name,
-.jeremy:hover .name {
+
+.jack:hover .name {
   color: black;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,17 +3,7 @@ import App from "./App.vue";
 import VueRouter from "vue-router";
 import VueResource from "vue-resource";
 import Home from "./components/Home.vue";
-import Geoff from "./components/Geoff/Geoff.vue";
-import GeoffCategories from "./components/Geoff/GeoffCategories.vue";
-import GeoffMapLoader from "./components/Geoff/map/GeoffMapLoader.vue";
-import GeoffMapCategories from "./components/Geoff/map/GeoffMapCategories";
-import GeoffFeatureLanding from "./components/Geoff/featured/GeoffFeatureLanding.vue";
-import GeoffBackBtn from "./components/Geoff/GeoffBackBtn.vue";
-import Holly from "./components/Holly/Holly.vue";
 import Jack from "./components/Jack/Jack.vue";
-import Jeremy from "./components/Jeremy/Jeremy.vue";
-import ProjectComponent from "./components/Jeremy/projectcomponent/ProjectComponent.vue";
-import SingleProject from "./components/Jeremy/singleproject/SingleProject.vue";
 
 Vue.config.productionTip = false;
 
@@ -22,28 +12,7 @@ Vue.use(VueResource);
 
 const routes = [
   { path: "/", component: Home },
-  { path: "/geoff", component: Geoff },
-  { path: "/geoffcategories", component: GeoffCategories },
-  { path: "/geoffmaploader", component: GeoffMapLoader, name: "geoffmaploader", props: true },
-  { path: "/geoffmapcategories", component: GeoffMapCategories, name: "geoffmapcategories", props: true },
-  { path: "/geofffeaturelanding", component: GeoffFeatureLanding, name: "geofffeaturelanding", props: true },
-  { path: "/geoffbackbtn", component: GeoffBackBtn },
-  { path: "/holly", component: Holly },
   { path: "/jack", component: Jack },
-  { path: "/jeremy", component: Jeremy},
-  {
-    name: "projectcomponent",
-    path: "/projectcomponent",
-    component: ProjectComponent,
-    props: true
-  },
-  {
-    name: "singleproject",
-    path: "/singleproject",
-    component: SingleProject,
-    props: true
-  },
-  
   { path: "/jkdesigners", component: JkDesigners},
   { path: "/jkprojects", component: JkProjects, name: "jkprojects", props: true },
   { path: "/jkuserproject", component: JkUserProject, name: "jkuserproject", props: true }
